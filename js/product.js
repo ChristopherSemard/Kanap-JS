f4e9d2b4f2902ee9858e03641d09224b4b22c1d1
ChristopherSemard <christophersemard76@gmail.com>
2022-03-29 10:51:18 +0200

    Page product

--- js/product.js
+++ js/product.js
@@ -34,5 +34,56 @@ function productBlock(data){
             <p>${data.description}</p>
             <p class="price">${data.price} €</p>
         </div>
-    </article>`
+    </article>
+    <form>
+        <h3>Sélectionnez la couleur</h3>
+        <div class="colorBlocks">${colorsBlocks}</div>
+        <div id="alertColor"></div>
+        <h3>Sélectionnez la quantité</h3>
+        <div id="quantity-selector">
+            <span id="moins" onclick="decreaseCount()" >-</span>
+            <input type="number" id="quantity" value="1">
+            <span id="plus" onclick="increaseCount()" >+</span>
+        </div>
+        <input id="send" onclick="addBasket()" value="Ajouter au panier" readonly/>
+    </form>
+    `
+}
+
+
+document.addEventListener("click", event => {
+    const target = event.target;
+    if (target.className.includes('color') && !target.className.includes('colorBlocks'))  {
+        const radioButtons = document.getElementsByClassName("color")
+        for(button of radioButtons){
+            button.classList.remove("active");
+        }
+        target.classList.toggle("active");
+    }
+})
+
+function decreaseCount(){
+    const quantityValue = document.getElementById("quantity")
+    if(quantityValue.value == 1){
+        return
+    }
+    else{
+        quantityValue.value = parseInt(quantityValue.value)-1
+    }
+}
+
+
+function increaseCount(){
+    const quantityValue = document.getElementById("quantity")
+    if( quantityValue.value < 101){
+        quantityValue.value = parseInt(quantityValue.value)+1
+    }
+    else{
+        return
+    }
+}
+
+
+function addBasket(){
+    
 }
